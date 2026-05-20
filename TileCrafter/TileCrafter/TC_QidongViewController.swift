98e72cb00bdc6613cd657295b1574484ae608932
jduja <jduja681@gmail.com>
2026-05-20 08:44:28 +0800

    1

--- TileCrafter/TileCrafter/TC_QidongViewController.swift
+++ TileCrafter/TileCrafter/TC_QidongViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
 import SwiftUI
 import Reachability
-import Ointts
 import AppTrackingTransparency
 
 final class TC_QidongViewController: UIViewController {
@@ -49,7 +48,6 @@ final class TC_QidongViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
 
         bxut.overrideUserInterfaceStyle = .dark
@@ -57,10 +55,10 @@ final class TC_QidongViewController: UIViewController {
         view.addSubview(bxut.view)
         bxut.view.translatesAutoresizingMaskIntoConstraints = false
         
-        let vpianes = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
-        vpianes!.view.tag = 811
-        vpianes?.view.frame = UIScreen.main.bounds
-        view.addSubview(vpianes!.view)
+//        let vpianes = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
+//        vpianes!.view.tag = 811
+//        vpianes?.view.frame = UIScreen.main.bounds
+//        view.addSubview(vpianes!.view)
 
         NSLayoutConstraint.activate([
             bxut.view.topAnchor.constraint(equalTo: view.topAnchor),
@@ -71,16 +69,16 @@ final class TC_QidongViewController: UIViewController {
 
         bxut.didMove(toParent: self)
 
-        let duye = try! Reachability()
-        duye.whenReachable = { reachability in
-            let usye = WidokGry(kontroler: KontrolerGry())
+//        let duye = try! Reachability()
+//        duye.whenReachable = { reachability in
+//            let usye = ChronoExpanseView(frame: self.view.bounds)
 //            let vuuis = UIView()
 //            vuuis.addSubview(usye)
-            duye.stopNotifier()
-        }
-        do {
-            try duye.startNotifier()
-        } catch {}
+//            duye.stopNotifier()
+//        }
+//        do {
+//            try duye.startNotifier()
+//        } catch {}
     
     }
 }
@@ -92,7 +90,7 @@ final class Ixouex {
     static let shared = Ixouex()
 
     private let monitor = NWPathMonitor()
-    private let queue = DispatchQueue(label: "com.resta.RestaurantMau", qos: .background)
+    private let queue = DispatchQueue(label: "com.tilec.TileCrafter", qos: .background)
     private var callback: ((Bool) -> Void)?
     private var started = false
 
